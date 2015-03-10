1ecfe6f40e60a15216ce3a26651832022c454c0a
SCurley3465 <05181998>
2015-03-10 08:46:17 -0700

    Added a space

--- src/main/java/Eden/Schedule.java
+++ src/main/java/Eden/Schedule.java
@@ -19,7 +19,7 @@ public class Schedule extends BukkitRunnable {
         FileConfiguration config = edenplugin.sendConfig();
         ArrayList<String> announcements = new ArrayList<>();
         announcements.addAll(config.getStringList("announcements"));
-        Bukkit.getServer().broadcastMessage(ChatColor.AQUA + "[BROADCAST]" + ChatColor.GREEN + announcements.get(inc++));
+        Bukkit.getServer().broadcastMessage(ChatColor.AQUA + "[BROADCAST] " + ChatColor.GREEN + announcements.get(inc++));
         if(inc>announcements.size()-1) inc = 0;
     }
 }
